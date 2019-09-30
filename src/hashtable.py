3177824c785047c4a447b0354afea3fbd0a1f271
rmejia-tajc <rmejia.tajc@gmail.com>
2019-09-30 14:35:22 -0600

    complete resize function

--- src/hashtable.py
+++ src/hashtable.py
@@ -16,6 +16,8 @@ class HashTable:
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
 
+        self.count = 0
+
 
     def _hash(self, key):
         '''
@@ -84,7 +86,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        new_storage = [None] * self.capacity
+        for i in range(self.count):
+            new_storage[i] = self.storage[i]
+
+        self.storage = new_storage
 
 
 
